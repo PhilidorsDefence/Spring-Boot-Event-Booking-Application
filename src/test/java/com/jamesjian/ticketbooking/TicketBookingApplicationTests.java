6972e6d24dd0f119a6199f9e1ef513d49131e0d5
james-townsend-jian <pen.par96@gmail.com>
2023-03-14 12:25:50 -0500

    Adding Spring Initializr generated Spring Boot project.

---
+++ src/test/java/com/jamesjian/ticketbooking/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesjian.ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

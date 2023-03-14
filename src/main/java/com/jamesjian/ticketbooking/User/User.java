3562af268b7459c7b7e53419f8a2673d154f23e1
james-townsend-jian <pen.par96@gmail.com>
2023-03-14 14:56:39 -0500

    Added User entity class.

---
+++ src/main/java/com/jamesjian/ticketbooking/User/User.java
@@ -0,0 +1,67 @@
+package com.jamesjian.ticketbooking.User;
+
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password should have at least 6 characters")
+    private String password;
+
+    public User(String name, String email, String password) {
+        name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
